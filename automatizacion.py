227c452ed7690b321b118f0aa652bc1070b0c7b6
lmonsalve22 <lmonsalve22@gmail.com>
2022-02-15 17:18:19 -0300

    Update automatizacion.py

--- automatizacion.py
+++ automatizacion.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from selenium import webdriver, Service
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.firefox.options import Options
