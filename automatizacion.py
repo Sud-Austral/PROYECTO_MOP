234279b64cc8e5945f8820632289396ce6cd538d
lmonsalve22 <lmonsalve22@gmail.com>
2022-02-15 16:41:06 -0300

    Update automatizacion.py

--- automatizacion.py
+++ automatizacion.py
@@ -30,13 +30,13 @@ def getDriver():
 def proceso():
     print("Comenzamos")
     driver = getDriver()
-    btndescarga = driver.find_element_by_xpath('/html/body/app-root/app-home/div/app-filter/div/section/button[2]')
-    btndescarga.click()
-    print(btndescarga.text)
-    btngeojson = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/div/div/button[3]')
-    btngeojson.click()
-    time.sleep(30)
-    print("Terminamos")
+    #btndescarga = driver.find_element_by_xpath('/html/body/app-root/app-home/div/app-filter/div/section/button[2]')
+    #btndescarga.click()
+    #print(btndescarga.text)
+    #btngeojson = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/div/div/button[3]')
+    #btngeojson.click()
+    #time.sleep(30)
+    #print("Terminamos")
     return
 
 
