1532ad529023c5cc8ef57c1a03afe41584151b05
lmonsalve22 <lmonsalve22@gmail.com>
2022-02-16 16:01:56 -0300

    Update automatizacion.py

--- automatizacion.py
+++ automatizacion.py
@@ -52,8 +52,8 @@ def proceso():
         print("Aca1")
         btngeojson = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/div/div/button[3]')
         btngeojson.click()
-        error = sys.exc_info()[1]
-        print(error)
+        print("Descarga Hecha")
+        
     except:
         print("Aca2")
         btngeojson = driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div/div/button[3]')
